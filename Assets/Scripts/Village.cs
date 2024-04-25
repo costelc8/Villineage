b882b9489184c62618c2fe253cfe83052f0aa262
abby-eds <86379188+abby-eds@users.noreply.github.com>
2024-04-24 19:53:00 -0700

    point on navmesh

--- Assets/Scripts/Village.cs
+++ Assets/Scripts/Village.cs
@@ -7,18 +7,36 @@ public class Village : MonoBehaviour
     private Dictionary<Resource, int> resources = new Dictionary<Resource, int>();
     public GameObject villagerPrefab;
     public List<Villager> villagers;
+    private RandomNavmeshPoint randomPoint;
 
     // Start is called before the first frame update
     void Start()
     {
+        randomPoint = new RandomNavmeshPoint();
+
         // Spawn 5 villagers for testing selection
         for (int i = 0; i < 5; i++) SpawnVillager();
+
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            // test grabbing a point on the navmesh
+            Vector3 point;
+            if (randomPoint.RandomPoint(transform.position, 5, out point))
+            {
+                // got a point
+            }
+            else
+            {
+                // failed
+            }
+        }
     }
 
     public void SpawnVillager()
