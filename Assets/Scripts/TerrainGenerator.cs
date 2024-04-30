60884187914d146b9b3834f71a82854c0799d343
abby-eds <86379188+abby-eds@users.noreply.github.com>
2024-04-29 18:22:44 -0700

    terrain???

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int depth = 20;
+    public int width = 200;
+    public int length = 200;
+    public float scale = 20f;
+
+    private void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        transform.position = new Vector3(-width/2, 0, -length/2);
+    }
+
+    TerrainData GenerateTerrain(TerrainData data)
+    {
+        data.heightmapResolution = width + 1;
+        data.size = new Vector3(width, depth, length);
+        data.SetHeights(0, 0, GeneratePerlin());
+        return data;
+    }
+
+    float[,] GeneratePerlin()
+    {
+        float[,] perlin = new float[width, length];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                perlin[x, y] = CalculatePerlin(x, y);
+            }
+        }
+        return perlin;
+    }
+
+    float CalculatePerlin(int x, int y)
+    {
+        float xCoord = (float)x / width * scale;
+        float yCoord = (float)y / length * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
