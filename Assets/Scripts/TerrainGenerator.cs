c86af2c878fb2fb3d12a569d7f7ed69ae1fb5ebc
giddinl2 <giddinl2@wwu.edu>
2024-04-30 11:00:46 -0700

    Terrain things

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,38 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using Unity.AI.Navigation;
 using UnityEngine;
 
+[RequireComponent(typeof(NavMeshSurface))]
 public class TerrainGenerator : MonoBehaviour
 {
+    private Terrain terrain;
     public int depth = 8;
-    private int width;
-    private int length;
+    private int size;
+    public int seed;
+    private float randomX;
+    private float randomY;
     public float scale = 10f;
 
     private void Start()
     {
-        Terrain terrain = GetComponent<Terrain>();
-        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        terrain = GetComponent<Terrain>();
+        GenerateTerrain(terrain.terrainData);
         NavMeshSurface surface = GetComponent<NavMeshSurface>();
         surface.BuildNavMesh();
     }
 
-    TerrainData GenerateTerrain(TerrainData data)
+    void GenerateTerrain(TerrainData data)
     {
-        width = data.heightmapResolution - 1;
-        length = width;
-        data.size = new Vector3(width, depth, length);
+        size = data.heightmapResolution;
+        data.size = new Vector3(size, depth, size);
+        GenerateRandomOffsets();
         data.SetHeights(0, 0, GeneratePerlin());
-        return data;
     }
 
     float[,] GeneratePerlin()
     {
-        float[,] perlin = new float[width, length];
-        for (int x = 0; x < width; x++)
+        float[,] perlin = new float[size, size];
+        for (int x = 0; x < size; x++)
         {
-            for (int y = 0; y < length; y++)
+            for (int y = 0; y < size; y++)
             {
                 perlin[x, y] = CalculatePerlin(x, y);
             }
@@ -42,9 +46,25 @@ public class TerrainGenerator : MonoBehaviour
 
     float CalculatePerlin(int x, int y)
     {
-        float xCoord = (float)x / width * scale;
-        float yCoord = (float)y / length * scale;
+        float xCoord = x / (size * scale / 10) + randomX;
+        float yCoord = y / (size * scale / 10) + randomY;
 
         return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+    void GenerateRandomOffsets()
+    {
+        if (seed == 0) seed = Random.Range(int.MinValue, int.MaxValue);
+        Random.State state = Random.state; // Save Random's state
+        Random.InitState(seed); // Seed Random's state
+        randomX = Random.Range(-10000 * scale, 10000 * scale);
+        randomY = Random.Range(-10000 * scale, 10000 * scale);
+        Random.state = state; // Restore Random's state
+    }
+
+    private void OnValidate()
+    {
+        size = GetComponent<Terrain>().terrainData.heightmapResolution;
+        transform.position = new Vector3(size / -2f, depth / -2f, size / -2f);
     }
 }
