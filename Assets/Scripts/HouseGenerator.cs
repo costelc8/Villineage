b1eb0613217e490cf5413fe172cf833a818fad08
abby-eds <86379188+abby-eds@users.noreply.github.com>
2024-05-01 11:06:37 -0700

    aaaa

--- Assets/Scripts/HouseGenerator.cs
+++ Assets/Scripts/HouseGenerator.cs
@@ -8,6 +8,7 @@ public class HouseGenerator : MonoBehaviour
     public TerrainGenerator terrainGen;
     Vector3 center;
     Vector3 houseSize;
+    Vector3 halfHouse;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,16 @@ public class HouseGenerator : MonoBehaviour
         Camera.main.transform.parent.parent.position = transform.position;
         center = new Vector3(terrainGen.size/2, 0, terrainGen.size/2);
         Vector3 houseSize = housePrefab.GetComponent<BoxCollider>().size;
+        halfHouse = new Vector3(houseSize.x / 2, houseSize.y / 2, houseSize.z / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
             // test grabbing a point on the navmesh
-            if (RandomNavmeshPoint.RandomPointFromCenterBox(center, houseSize, out Vector3 point, 5f, 5f, 1000f))
+            if (RandomNavmeshPoint.RandomPointFromCenterBox(center, halfHouse, out Vector3 point, 5f, 5f, 1000f))
             {
                 // got a point
                 print("house");
@@ -32,7 +34,10 @@ public class HouseGenerator : MonoBehaviour
             else
             {
                 // failed
+                print("house failure");
             }
+        }
+            
         
     }
 }
