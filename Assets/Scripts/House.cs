eba0b805ac42093572edfba0b7360088cd220449
abby-eds <86379188+abby-eds@users.noreply.github.com>
2024-05-01 18:36:17 -0700

    houseeeeee

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
-    private Transform construction;
-    private Transform finished;
+    private Transform house0;
+    private Transform house1;
+    private Transform house2;
 
-    public bool built = false;
+    [Range(0, 2)]
+    public int state = 0;
     // Start is called before the first frame update
     void Start()
     {
-        construction = transform.Find("Construction");
-        finished = transform.Find("Finished");
+        house0 = transform.Find("House_0");
+        house1 = transform.Find("House_1");
+        house2 = transform.Find("House_2");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (built)
+        if (state == 0)
         {
-            construction.gameObject.SetActive(false);
-            finished.gameObject.SetActive(true);
-        } 
-        else
-        {
-            construction.gameObject.SetActive(true);
-            finished.gameObject.SetActive(false);
+            house0.gameObject.SetActive(true);
+            house1.gameObject.SetActive(false);
+            house2.gameObject.SetActive(false);
         }
+        else if (state == 1)
+        {
+            house0.gameObject.SetActive(false);
+            house1.gameObject.SetActive(true);
+            house2.gameObject.SetActive(false);
+        }
+        else if (state == 2)
+        {
+            house0.gameObject.SetActive(false);
+            house1.gameObject.SetActive(false);
+            house2.gameObject.SetActive(true);
+        }
+       
     }
+    
 }
