cd6297a7005a614f5372871f59ea66c01584de89
abby-eds <86379188+abby-eds@users.noreply.github.com>
2024-05-29 10:39:25 -0700

    maybe fix roads actually

--- Assets/Scripts/RoadPainter.cs
+++ Assets/Scripts/RoadPainter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RoadPainter : MonoBehaviour
 {
@@ -9,23 +10,45 @@ public class RoadPainter : MonoBehaviour
     private Vector3 terrainPos;
     private float[,,] map;
     private Vector3 lastPosition;
-    private Vector3 position; 
+    private Vector3 position;
+    private NavMeshAgent agent;
+    private bool drawing;
 
     // Start is called before the first frame update
     void Start()
     {
         terrain = TerrainGenerator.terrain;
         terrainData = terrain.terrainData;
-       
+        agent = GetComponent<NavMeshAgent>();
+
         int textureCount = terrainData.alphamapLayers;
         map = new float[1, 1, textureCount];
         lastPosition = transform.position;
         position = lastPosition;
+        drawing = false;
+
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        if (agent.speed > 0 && !drawing)
+        {
+            // moving
+            drawing = true;
+            InvokeRepeating("DrawPath", 0, 4);
+
+        } 
+        else if (agent.speed == 0 && drawing)
+        {
+            // stopped
+            CancelInvoke("DrawPath");
+            drawing = false;
+        }
 
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void DrawPath()
     {
         position = transform.position;
         int lastX = (int)lastPosition.x;
@@ -36,12 +59,16 @@ public class RoadPainter : MonoBehaviour
         if (x != lastX || y != lastY)
         {
             float[,,] currentMap = terrainData.GetAlphamaps(x, y, 1, 1);
-            map[0, 0, 0] = currentMap[0, 0, 0] - 0.1f;
-            map[0, 0, 1] = currentMap[0, 0, 1] + 0.1f;
-            terrainData.SetAlphamaps((int)position.x, (int)position.z, map);
-       
+            float grass = currentMap[0, 0, 0];
+            if (grass > 0f)
+            {
+                // not fully sand
+                grass -= 0.05f;
+                map[0, 0, 0] = grass;
+                map[0, 0, 1] = 1 - grass;
+                terrainData.SetAlphamaps((int)position.x, (int)position.z, map);
+            }
         }
         lastPosition = position;
-
     }
 }
